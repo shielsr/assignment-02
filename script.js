575a76dc66e2d2a58142e4d4b7fed02546d67586
Rob Shiels <rob@robshiels.com>
2025-07-19 18:56:19 +0100

    Building the test function to pull in dropdown values

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function submitMessage() { 
-    document.getElementById("formFeedback").innerHTML = "Thank you for submiting. ";
+
+
+
+function allValues() {
+const redValue = Number(document.getElementById("redQuantity").value);
+
+const yellowValue = Number(document.getElementById("yellowQuantity").value);
+
+const valuesTotal = redValue + yellowValue;
+
+document.getElementById("totalSweets").innerText = valuesTotal;
 }
+
+
+//Array
+const valuesTotal = [
+    redValue, yellowValue
+];
+
+
+document.getElementById("id-of-form").addEventListner("submit", allValues);
